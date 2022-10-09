858c2fbbcb5b0ef51245d61105d496507f197391
koneki88 <114171959+koneki88@users.noreply.github.com>
2022-10-09 18:13:32 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,51 @@
+//1
+/*int hp;
+ cin >> hp;
+ bool r;
+ r = hp > 0;
+ cout << boolalpha << r;*/
+ //2 
+//int a;
+//cin >> a;
+//if (a > 20) {
+//	cout << "on";
+//}
+//else {
+//	cout << "off";*//
+
+ //3
+/*int tempP,tempK,vl;
+ cin >> tempP >> tempK >> vl;
+ if ((tempP > tempK) and (vl<80)){
+  cout << "on";
+ }
+ else {
+  cout << "off";
+ }*/
+
+
+//4
+//int main() {
+//	setlocale(LC_ALL, "Russian");
+//	double y, x;
+//	cin >> x;
+//	if (x > 0) {
+//		y = sqrt(x - 2);
+//	}
+//	if (x = 0) {
+//		y = 0;
+//	}
+//	if (x < 0) {
+//		y = abs(0);
+//	}
+//	cout << y << endl;
+
+
+
+
+
+
+
+
+}
+
